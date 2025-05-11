67fd197d5a305a5552a7dffd612108d3915c54c6
mp <thanat101@gmail.com>
2025-05-10 21:07:27 -0400

    Initial Commit

---
+++ AnubisSDS/AnubisSDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnubisSDSApp.swift
+//  AnubisSDS
+//
+//  Created by Mark Pfeifer on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnubisSDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnubisSDS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnubisSDS
+//
+//  Created by Mark Pfeifer on 5/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
